aac24edd8276eca956717c61ab82a9f3036e645f
hotshot07 <aroram@tcd.ie>
2023-02-25 19:16:55 +0000

    hehe

--- src/helloflask.py
+++ src/helloflask.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, redirect
 import manager
 
 app = Flask(__name__)
@@ -8,9 +8,15 @@ def get_short_link(shortlink : str):
     
     # make call to manager to return longlinks
     
-    manager.get_link_from_database(shortlink) 
+    longlink = manager.get_link_from_database(str(shortlink)) 
     
-    return 'longlink'
+    if longlink == 'ohno':
+        return Response("me dumb", status=400)
+    
+    if longlink == 'NotFound':
+        return Response("be link does not exist", status=404)
+    
+    return redirect(longlink, code=302)
     
     
 @app.route('/create', methods=['POST'])
--- src/manager.py
+++ src/manager.py
@@ -1,4 +1,6 @@
 import mysql.connector
+import time
+
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -8,22 +10,26 @@ mydb = mysql.connector.connect(
 )
 
 
-def get_link_from_database(shortlink):
+def get_link_from_database(shortlink:str) -> str:
     
-    print(shortlink)
     mycursor = mydb.cursor()
     
-    mycursor.execute("SHOW DATABASES")
-    
-    for x in mycursor:
-        print(x)
-    
-    
-    
-    # get links from the shortlink table
-    # write to timestamp in analytics table  
-    
-    return None
+    try:
+        mycursor.execute(f"SELECT longlink FROM shortlink where `shortlink` = '{shortlink}'")
+        longlink = mycursor.fetchall()
+        if (len(longlink) == 0):
+            return "NotFound"
+        
+        mycursor.execute(f"INSERT INTO analytics (`shortLink`, `timestamp`) VALUES ('{shortlink}', {int(time.time())});")
+        mydb.commit()
+        
+        return longlink[0][0]
+        
+    except Exception as e:
+        print(e)
+        return 'ohno'
+
+# [('https://www.google.com',)] 
 
 
 def create_link_in_database(link_dictionary : dict) -> str:
